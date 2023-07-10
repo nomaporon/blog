7c859675dd1fac95b3106e7612b1242e25999968
nomaporon <masaharu.ono23642@gmail.com>
2023-07-10 15:06:50 +0900

    認証機能の追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,42 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-
-        <title>Blog</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body class="antialiased">
-        <h1>Blog Name</h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-            <div class='post'>
-                <h2 class='Title'>
-                    <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
-                </h2>
-                <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-                <p class='body'>{{ $post->body }}</p>
-                <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
-                    @csrf
-                    @method('DELETE')
-                    <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
-                </form>
+    <x-app-layout>
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Index') }}
+            </h2>
+        </x-slot>
+        <body class="antialiased">
+            <h1>Blog Name</h1>
+            <div class='posts'>
+                @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='Title'>
+                        <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+                    <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
+                </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
-        <a href="/posts/create">create</a>
-        <div class='paginate'>{{ $posts->links() }}</div>
-        <script>
-            function deletePost(id) {
-                'use strict'
-                
-                if (confirm('削除すると復元できません。\n 本当に削除しますか？')) {
-                    document.getElementById(`form_${id}`).submit();
+            <a href="/posts/create">create</a>
+            <div>ログインユーザー:{{ Auth::user()->name }}</div>
+            <div class='paginate'>{{ $posts->links() }}</div>
+            <script>
+                function deletePost(id) {
+                    'use strict'
+                    
+                    if (confirm('削除すると復元できません。\n 本当に削除しますか？')) {
+                        document.getElementById(`form_${id}`).submit();
+                    }
                 }
-            }
-        </script>
-    </body>
+            </script>
+        </body>
+    </x-app-layout>
 </html>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-
-        <title>Blog</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    </head>
-    <body class="antialiased">
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class='content'>
-            <div class='content_post'>
-                <h3>本文</h3>
-                <p class='body'>{{ $post->body }}</p>
+    <x-app-layout>
+        <x-slot name="header">
+            <head>
+                <meta charset="utf-8">
+        
+                <title>Blog</title>
+        
+                <!-- Fonts -->
+                <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        
+            </head>
+        </x-slot>
+        <body class="antialiased">
+            <h1 class="title">
+                {{ $post->title }}
+            </h1>
+            <div class='content'>
+                <div class='content_post'>
+                    <h3>本文</h3>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
             </div>
-        </div>
-        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
-        <div class="edit">
-            <a href="/posts/{{ $post->id }}/edit">編集</a>
-        </div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
-    </body>
+            <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+            <div class="edit">
+                <a href="/posts/{{ $post->id }}/edit">編集</a>
+            </div>
+            <div class="footer">
+                <a href="/">戻る</a>
+            </div>
+        </body>
+    </x-app-layout>
 </html>
